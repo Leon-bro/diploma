ab8109edde777a24909d4aabbdd4ddea3a69e355
roman <roman.bryzhatiy@gmail.com>
2024-10-22 08:27:51 +0300

    added Mcc

--- custom_metrics.py
+++ custom_metrics.py
@@ -10,7 +10,7 @@ def iou_metric(y_true, y_pred, smooth=1e-6):
     return (intersection + smooth) / (union + smooth)
 
 class DiceCoefficient(tf.keras.metrics.Metric):
-    def __init__(self, name='dice_coefficient', **kwargs):
+    def __init__(self, name='Dice', **kwargs):
         super(DiceCoefficient, self).__init__(name=name, **kwargs)
         self.true_positives = self.add_weight(name='tp', initializer='zeros')
         self.false_positives = self.add_weight(name='fp', initializer='zeros')
@@ -35,3 +35,47 @@ class DiceCoefficient(tf.keras.metrics.Metric):
         self.true_positives.assign(0)
         self.false_positives.assign(0)
         self.false_negatives.assign(0)
+
+class Mcc(tf.keras.metrics.Metric):
+    def __init__(self, name='Mcc', **kwargs):
+        super(Mcc, self).__init__(name=name, **kwargs)
+        # Initialize state variables
+        self.tp = self.add_weight(name='tp', initializer='zeros')
+        self.tn = self.add_weight(name='tn', initializer='zeros')
+        self.fp = self.add_weight(name='fp', initializer='zeros')
+        self.fn = self.add_weight(name='fn', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # Convert predictions to binary
+        y_pred = tf.round(tf.clip_by_value(y_pred, 0, 1))
+
+        # Cast to float for summing
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+
+        # Calculate the confusion matrix components
+        tp = tf.reduce_sum(y_true * y_pred)
+        tn = tf.reduce_sum((1 - y_true) * (1 - y_pred))
+        fp = tf.reduce_sum((1 - y_true) * y_pred)
+        fn = tf.reduce_sum(y_true * (1 - y_pred))
+
+        # Update the state variables
+        self.tp.assign_add(tp)
+        self.tn.assign_add(tn)
+        self.fp.assign_add(fp)
+        self.fn.assign_add(fn)
+
+    def result(self):
+        # Compute the Matthews correlation coefficient (MCC)
+        numerator = (self.tp * self.tn) - (self.fp * self.fn)
+        denominator = tf.sqrt((self.tp + self.fp) * (self.tp + self.fn) * (self.tn + self.fp) * (self.tn + self.fn))
+
+        # Avoid division by zero
+        return tf.where(tf.equal(denominator, 0), 0.0, numerator / denominator)
+
+    def reset_states(self):
+        # Reset the state variables at the end of an epoch
+        self.tp.assign(0)
+        self.tn.assign(0)
+        self.fp.assign(0)
+        self.fn.assign(0)
